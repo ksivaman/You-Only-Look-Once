e69b8a936b5deeca2b37584e8bf4edb11a14bed2
Kirthi Shankar Sivamani <ksivaman@ee220clnx1.ecn.purdue.edu>
2019-03-10 16:20:00 -0400

    created model architecture

--- darknet.py
+++ darknet.py
@@ -4,28 +4,30 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.autograd as Variable
-import numpy as 
+import numpy as np
 
 def parse_cfg(cfgfile):
-   file = open(cfgfile, 'r')
-   lines = [x for x in lines if len(x) > 0]
-   lines = [x for x in lines if x[0] != '#']
-   lines = [x.rstrip().lstrip() for x in lines]
 
-   block = {}
-   blocks = []
-
-   for line in lines:
-       if line[0] == "[":
-           if len(block) != 0:
-               blocks.append(block)
-               block = {}
-            block["type"] = line[1:-1].rstrip()
+    file = open(cfgfile, 'r')
+    lines = file.read().split('\n')
+    lines = [x for x in lines if len(x) > 0]
+    lines = [x for x in lines if x[0] != '#'] 
+    lines = [x.rstrip().lstrip() for x in lines]          
+    
+    block = {}
+    blocks = []
+    
+    for line in lines:
+        if line[0] == "[":               
+            if len(block) != 0:          
+                blocks.append(block)     
+                block = {}               
+            block["type"] = line[1:-1].rstrip()     
         else:
-            key,value = line.split("=")
+            key,value = line.split("=") 
             block[key.rstrip()] = value.lstrip()
     blocks.append(block)
-
+    
     return blocks
 
 def create_modules(blocks):
@@ -82,5 +84,52 @@ def create_modules(blocks):
 
             try:
                 end = int(x["layers"][1])
-            except
+            except:
                 end = 0
+
+            if start > 0: 
+                start = start - index
+            if end > 0:
+                end = end - index
+
+            route = EmptyLayer()
+            module.add_module("route_{0}".format(index), route)
+
+            if end < 0:
+                filters = output_filters[index + start] + output_filters[index + end]
+            else:
+                filters= output_filters[index + start]
+
+        elif x["type"] == "shortcut":
+            shortcut = EmptyLayer()
+            module.add_module("shortcut_{}".format(index), shortcut)
+
+        elif x["type"] == "yolo":
+            mask = x["mask"].split(",")
+            mask = [int(x) for x in mask]
+
+            anchors = x["anchors"].split(",")
+            anchors = [int(a) for a in anchors]
+            anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors),2)]
+            anchors = [anchors[i] for i in mask]
+
+            detection = DetectionLayer(anchors)
+            module.add_module("Detection_{}".format(index), detection)
+
+        module_list.append(module)
+        prev_filters = filters
+        output_filters.append(filters)
+    
+    return (net_info, module_list)
+
+class DetectionLayer(nn.Module):
+    def __init__(self, anchors):
+        super(DetectionLayer, self).__init__()
+        self.anchors = anchors
+
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+
+blocks = parse_cfg('cfg/yolov3.cfg')
+print(create_modules(blocks))
