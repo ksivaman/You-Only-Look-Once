45c76545d3bd4d9da1d513cc378c20c18d51c209
ksivaman <ksivaman@purdue.edu>
2019-04-10 23:08:22 -0400

    model to cuda

--- detector.py
+++ detector.py
@@ -38,11 +38,11 @@ def arg_parse():
                         default = "416", type = str)
     
     return parser.parse_args()
-    
+
+start = 0 
 args = arg_parse()
 images = args.images
 batch_size = int(args.bs)
 confidence = float(args.confidence)
 nms_thesh = float(args.nms_thresh)
-start = 0
 CUDA = torch.cuda.is_available()
